c68723bda3609e67ac896ba46319cb2a157ea92d
Vitalii Martyniak <vitalldh@gmail.com>
2021-03-12 15:38:06 +0200

    fix: add running application via daphne

--- spoonbill_web/asgi.py
+++ spoonbill_web/asgi.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
 
 import os
 
-from channels.auth import AuthMiddlewareStack
+# from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 
@@ -20,6 +20,6 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "spoonbill_web.settings.settings
 application = ProtocolTypeRouter(
     {
         "http": get_asgi_application(),
-        "websocket": AuthMiddlewareStack(URLRouter(core.routing.websocket_urlpatterns)),
+        "websocket": URLRouter(core.routing.websocket_urlpatterns),
     }
 )
