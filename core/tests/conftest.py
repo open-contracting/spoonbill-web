e8750c740cf19fa584b3537fccd4de220a29d93c
Vitalii Martyniak <vitalldh@gmail.com>
2021-03-12 15:08:23 +0200

    tests: add tests for project

---
+++ core/tests/conftest.py
@@ -0,0 +1,52 @@
+import os
+import uuid
+
+import pytest
+from django.utils import timezone
+
+from core.models import Upload, Validation
+
+
+class Task:
+
+    @property
+    def id(self):
+        return str(uuid.uuid4())
+
+
+@pytest.fixture
+def dataset():
+    path = os.path.dirname(__file__) + "/data/don.json"
+    file_ = open(path)
+    yield file_
+
+    file_.close()
+
+
+@pytest.fixture
+def validation_task(mocker):
+    mock = mocker.patch('core.views.validate_data')
+    mock.delay.return_value = Task()
+    return mock
+
+
+@pytest.fixture
+def cleanup_upload_task(mocker):
+    mock = mocker.patch('core.views.cleanup_upload')
+    return mock
+
+
+@pytest.fixture
+def mocked_sleep(mocker):
+    mock = mocker.patch('core.tasks.time.sleep')
+    return mock
+
+
+@pytest.fixture
+def validation_obj():
+    return Validation.objects.create()
+
+
+@pytest.fixture
+def upload_obj(validation_obj):
+    return Upload.objects.create(filename='don.json', validation=validation_obj, expired_at=timezone.now())
---
+++ core/tests/test_tasks.py
@@ -0,0 +1,46 @@
+from datetime import timedelta
+
+import pytest
+from django.utils import timezone
+
+from core.models import Upload, Validation
+from core.tasks import cleanup_upload, validate_data
+
+
+@pytest.mark.django_db
+class TestValidateDataTask:
+
+    def test_success(self, mocked_sleep):
+        validation = Validation.objects.create()
+        upload = Upload.objects.create(filename='don.json', validation=validation)
+
+        assert upload.validation.is_valid is None
+
+        validate_data(upload.id, validation.id)
+
+        upload = Upload.objects.get(id=upload.id)
+        assert upload.validation.is_valid
+
+
+@pytest.mark.django_db
+class TestCleanupUploadTask:
+
+    def test_success(self):
+        expired_at = timezone.now()
+        validation = Validation.objects.create()
+        upload = Upload.objects.create(filename='don.json', validation=validation, expired_at=expired_at)
+        assert not upload.deleted
+
+        cleanup_upload(upload.id)
+        upload = Upload.objects.get(id=upload.id)
+        assert upload.deleted
+
+    def test_skip_cleanup(self):
+        expired_at = timezone.now() + timedelta(minutes=1)
+        validation = Validation.objects.create()
+        upload = Upload.objects.create(filename='don.json', validation=validation, expired_at=expired_at)
+        assert not upload.deleted
+
+        cleanup_upload(upload.id)
+        upload = Upload.objects.get(id=upload.id)
+        assert not upload.deleted
