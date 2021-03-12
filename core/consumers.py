8d9b6626faf41c787915001802ea849eff9e686c
Vitalii Martyniak <vitalldh@gmail.com>
2021-03-12 14:52:44 +0200

    refactor: rewrite ValidationConsumer as asynchronous

--- core/consumers.py
+++ core/consumers.py
@@ -1,28 +1,19 @@
 import json
 
-from asgiref.sync import async_to_sync
-from channels.generic.websocket import WebsocketConsumer
-from channels.layers import get_channel_layer
+from channels.generic.websocket import AsyncWebsocketConsumer
 
 
-class ValidationConsumer(WebsocketConsumer):
-    def connect(self):
-        self.accept()
+class ValidationConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
         self.room_name = self.scope["url_route"]["kwargs"]["upload_id"]
         self.room_group_name = f"validate_data_{self.room_name}"
 
-        async_to_sync(self.channel_layer.group_add)(
-            self.room_group_name, self.channel_name
-        )
+        await self.channel_layer.group_add(self.room_group_name, self.channel_name)
+        await self.accept()
 
-    def disconnect(self, close_code):
-        async_to_sync(self.channel_layer.group_discard)(
-            self.room_group_name, self.channel_name
-        )
+    async def disconnect(self, close_code):
+        await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
 
-    # def recieve(self, text_data):
-    #     data = json.loads(text_data)
-
-    def task_validate(self, event):
+    async def task_validate(self, event):
         message = event["message"]
-        self.send(text_data=json.dumps({"message": message}))
+        await self.send(text_data=json.dumps({"message": message}))
