55d68cc057b62a87d64cea2972f635e87fe14abe
oleh.chepak <oleh.chepak@quintagroup.org>
2021-03-25 15:13:44 +0200

    fix: tests for AppDropzone component

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -20,7 +20,12 @@ module.exports = {
     },
     overrides: [
         {
-            files: ['**/__tests__/*.{j,t}s?(x)', '**/tests/unit/**/*.spec.{j,t}s?(x)'],
+            files: [
+                'jest.setup.js',
+                '**/__mocks__/*.{j,t}s?(x)',
+                '**/__tests__/*.{j,t}s?(x)',
+                '**/tests/unit/**/*.spec.{j,t}s?(x)'
+            ],
             env: {
                 jest: true,
             },
--- frontend/jest.setup.js
+++ frontend/jest.setup.js
@@ -3,3 +3,15 @@ import Vuetify from 'vuetify';
 
 Vue.use(Vuetify);
 Vue.config.productionTip = false;
+
+jest.mock('@/services/ApiService', () => {
+    return {
+        sendFile: jest.fn(() => {
+            return {
+                data: {
+                    id: 'mocked_id',
+                },
+            };
+        }),
+    };
+});
