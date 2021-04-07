210bd4a2e7a84493caf32beb542052678b35248c
Vitalii Martyniak <vitalldh@gmail.com>
2021-04-07 08:58:30 +0300

    docs: replace docs theme alabaster to open-contracting

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,7 +13,11 @@
 # import os
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
+import os
+from glob import glob
+from pathlib import Path
 
+import standard_theme
 
 # -- Project information -----------------------------------------------------
 
@@ -46,7 +50,9 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "alabaster"
+html_theme = "standard_theme"
+html_theme_path = [standard_theme.get_html_theme_path()]
+html_favicon = "_static/favicon-16x16.ico"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
