1c02320e8ef4f63b4e2889af1296a133206b9209
Vitalii Martyniak <vitalldh@gmail.com>
2021-04-12 15:02:45 +0300

    fix: add LOCALE_PATHS to settings.py

--- spoonbill_web/settings/settings.py
+++ spoonbill_web/settings/settings.py
@@ -156,6 +156,7 @@ USE_L10N = True
 
 USE_TZ = True
 
+LOCALE_PATHS = [f"{BASE_DIR}/core/locale"]
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
