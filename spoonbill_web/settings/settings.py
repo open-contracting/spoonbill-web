f57e96b55f40d2561f53ed9ca63c1f390687b839
Vitalii Martyniak <vitalldh@gmail.com>
2021-04-13 16:11:48 +0300

    fix: transifex configuration

--- spoonbill_web/settings/settings.py
+++ spoonbill_web/settings/settings.py
@@ -160,8 +160,6 @@ USE_L10N = True
 
 USE_TZ = True
 
-LOCALE_PATHS = [f"{BASE_DIR}/core/locale"]
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
