ed20adfcf9f19417da36c24321c2588762b7d9bf
Andrii Romaniuk <marioboy@quintagroup.org>
2021-07-29 15:30:34 +0300

    fix: percentage over100

--- core/tests/test_utils.py
+++ core/tests/test_utils.py
@@ -1,8 +1,15 @@
+import os
+import pathlib
+
 import pytest
 from django.conf import settings
 
 from core.models import DataFile
-from core.utils import multiple_file_assigner
+from core.utils import gz_size, multiple_file_assigner
+
+DATA_DIR = os.path.dirname(__file__) + "/data"
+DATASET_PATH_GZ = f"{DATA_DIR}/sample-dataset.json.gz"
+DATASET_PATH = f"{DATA_DIR}/sample-dataset.json"
 
 
 @pytest.mark.django_db
@@ -16,3 +23,9 @@ def test_multiple_file_assigner():
     media_paths = [settings.MEDIA_ROOT + path for path in paths]
 
     assert file_paths == media_paths
+
+
+def test_gz_size():
+    gz_uncompressed_size = gz_size(DATASET_PATH_GZ)
+    non_gz_file_size = pathlib.Path(DATASET_PATH).stat().st_size
+    assert gz_uncompressed_size == non_gz_file_size
