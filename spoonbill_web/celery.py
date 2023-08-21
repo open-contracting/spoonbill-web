94be131968a8702cac8bcf72cfa848d0ff8eb4af
James McKinney <26463+jpmckinney@users.noreply.github.com>
2023-08-21 10:29:00 -0400

    chore: Configure Celery to avoid deprecation warnings

--- spoonbill_web/celery.py
+++ spoonbill_web/celery.py
@@ -4,6 +4,11 @@ from celery import Celery
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "spoonbill_web.settings")
 
-app = Celery("spoonbill_web")
+app = Celery(
+    "spoonbill_web",
+    broker_connection_retry=False,
+    broker_connection_retry_on_startup=True,
+    broker_channel_error_retry=True,
+)
 app.config_from_object("django.conf:settings", namespace="CELERY")
 app.autodiscover_tasks()
