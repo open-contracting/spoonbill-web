c8f28866c245488d34eb8702768fcf0de5a07bb7
James McKinney <26463+jpmckinney@users.noreply.github.com>
2024-12-03 19:31:51 -0500

    fix: Do global replace to escape [] in regular expression

--- frontend/src/utils/getQueryParam.js
+++ frontend/src/utils/getQueryParam.js
@@ -1,5 +1,5 @@
 export default (name) => {
-    name = name.replace(/[[]/, '\\[').replace(/[\]]/, '\\]');
+    name = name.replace(/[[]/g, '\\[').replace(/[\]]/g, '\\]');
     const regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
     const results = regex.exec(window.location.hash);
     return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
