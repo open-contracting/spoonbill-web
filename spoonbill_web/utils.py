bad5262fcaabfd3b3f4e033b8ae9d0eef9cf0dd9
James McKinney <26463+jpmckinney@users.noreply.github.com>
2026-04-11 10:51:45 -0400

    docs(django52): Update references to Django

--- spoonbill_web/utils.py
+++ spoonbill_web/utils.py
@@ -37,7 +37,7 @@ TABLES_ORDER = (
 )
 
 
-# https://docs.djangoproject.com/en/4.2/ref/models/fields/#django.db.models.FileField.upload_to
+# https://docs.djangoproject.com/en/5.2/ref/models/fields/#django.db.models.FileField.upload_to
 def instance_directory_path(instance, filename):  # noqa: ARG001
     # file will be uploaded to MEDIA_ROOT/<id>/<uuid>
     return f"{instance.id}/{uuid.uuid4().hex}.json"
